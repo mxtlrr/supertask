9c47f7bf90f75e66a3845459214c574c6146eb33
mxtlrr <ruson0914@gmail.com>
2023-06-07 21:18:10 -0500

    so i did the supertask

--- src/supertask.py
+++ src/supertask.py
@@ -5,3 +5,31 @@
 # f(x) = 1/(2^x)
 def f(x: int) -> float:
   return 1/(2**x)
+
+start = __import__("time").time()
+
+some_condition = (1==1)
+
+k0 = 1
+kn = k0
+
+iterations = 0
+timing: int = 1 # minutes
+
+while some_condition:
+  print(f"{kn}")
+  __import__("time").sleep(timing*60)
+  kn = kn/2
+  timing = timing/2
+
+  if kn == 0.0:
+    break
+
+  iterations += 1
+
+end = __import__("time").time()
+seconds = end-start
+minutes = seconds / 60
+
+print(f"\n\nFINISHED.\nIt took {iterations} iterations, and %d minutes elapsed."
+      % minutes)
